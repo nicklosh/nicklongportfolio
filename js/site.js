92ce56512401cde039ab747c70ab955774be75c8
nicklosh <nkmlong@gmail.com>
2013-11-11 09:31:59 +1100

    init project

---
+++ js/site.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+// Init	
+	fitScreen();
+
+// on window resize	
+	$(window).resize(function(){
+		fitScreen();
+	});
+
+// screen size function
+	function fitScreen(){
+	// Get window height	
+		var winHeight = $(window).height();
+	// if screen height < window height
+		// var screenHeights = $('.screen');
+		// for(i = 0, ii = screenHeights.length; i<ii; i++){
+		// 	if($(screenHeights[i]).height()<winHeight){
+		// 		$(screenHeights[i]).height(winHeight);
+		// 	}
+		// }
+
+	// just about me .sceen for small screen 
+		$("#aboutme").height(winHeight);
+	};
+
+// main nav 
+	$("#navList > a").on('click', function(event){
+		event.preventDefault();
+		// alert("this");
+		$("#navList ul").toggleClass("active");
+	});
+
+// image switcher
+	
+	// controls
+	$(".controls a").on('click', function(event){
+		event.preventDefault();
+	})
+});
