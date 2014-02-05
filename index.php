405b025e17766e6d351679b6e088ba5b1e782127
Nick <nkmlong@gmail.com>
2014-02-05 11:12:18 +1100

    contact form function

--- index.php
+++ index.php
@@ -1,4 +1,23 @@
 <?php
+if (isset($_POST['submit']))
+{
+	$to = 'nkmlong@gmail.com';
+	$from = $_POST['email']; 
+	$name = $_POST['name'];
+	$comment = $_POST['comment'];
+	$subject = 'Examples of work contact form'; 
+	$subject2 = 'Copy of your email'; 
+	$message = $name . " wrote the following: " . "\n\n" . $comment;
+	$message2 = "Here is a copy of your message " . $name . "\n\n" . $_POST['message'];
+
+	$headers = "From:" . $from;
+	$headers2 = "From:" . $to;
+	mail($to, $subject, $message, $headers);
+	mail($from, $subject2, $message2, $headers2);
+	include 'thankyou.html.php';
+	exit();
+ }
+
 include 'index.html.php';
 // if(!isset($_REQUEST['password'])) 
 // {
---
+++ thankyou.html.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Nick Long - Examples of work</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width">
+	<link rel="stylesheet" href="/css/normalize.min.css">
+	<link rel="stylesheet" href="/css/main.css">
+	<script src="/js/vendor/jquery-1.10.2.min.js"></script>
+	<script src="/js/site.js"></script>
+</head>
+<body>
+<div class="screen">
+	<div class="content">
+		<p>Thankyou <?php $_POST['name']?>, your message has been sent.</p>
+		<p>Return to <a href="">home page?</a></p>
+	</div>
+</div>
+</body>
+</html>
