6b2a315c70c43413913cd83d7cc31e630b3d69fc
Nick <nkmlong@gmail.com>
2014-02-26 12:13:32 +1100

    remove pword

--- index.php
+++ index.php
@@ -24,21 +24,4 @@ if (isset($_POST['submit']))
  }
 
 include 'index.html.php';
-// if(!isset($_REQUEST['password'])) 
-// {
-// 	include 'pword.html.php';
-// }
-// else 
-// {
-// 	$password = $_REQUEST['password'];
-// 	if($password == 'NOT THIS LOL')
-// 	{
-// 		include 'index.html.php';
-// 	}
-// 	else
-// 	{
-// 		$output = "<p>Sorry, that is not the correct password</p>";
-// 		include 'pword.html.php';
-// 	}
-// }
 ?>
